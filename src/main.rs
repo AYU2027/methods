7da948af6177170245f5356191401f9de15b4e3d
Ayush Sinha <ayush40451k@gmail.com>
2025-06-26 07:46:09 +0000

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-fn main(){
-    
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+}
 
+fn area(shape: Shape) {
+    match shape {
+        Shape::Circle(radius) => {
+            println!("Circle Area: {:.2}", std::f64::consts::PI * radius * radius);
+        }
+        Shape::Rectangle(width, height) => {
+            println!("Rectangle Area: {:.2}", width * height);
+        }
+    }
+}
+
+fn main() {
+    let c = Shape::Circle(3.0);
+    let r = Shape::Rectangle(4.0, 45.0);
+
+    area(c);
+    area(r);
 }
